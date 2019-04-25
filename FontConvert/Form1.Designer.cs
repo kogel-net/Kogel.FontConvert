4c2de14e97c19471f5e522037c355a2c1d403f67
935368322 <935368322@qq.com>
2019-04-25 09:42:03 +0800

    添加项目文件。

---
+++ FontConvert/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace FontConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "转繁体";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 211);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "浏览";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(86, 220);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "宋体转微软雅黑";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(192, 220);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "添加报表默认字体和大小";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(5, 249);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(149, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "报表字体调小1px";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 305);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ FontConvert/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace FontConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Multiselect = true;
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Clear();
+                this.textBox1.Text = string.Join(",", openfile.FileNames);
+            }
+        }
+        private void ReadFile(string filePath, Func<string, string> callback)
+        {
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                FileInfo file = new FileInfo(filePath);
+                StreamReader reader = file.OpenText();
+                string line;
+                //读取文本内容
+                while (null != (line = reader.ReadLine()))
+                {
+                    string newLine = callback(line);
+                    sb.Append(newLine + Environment.NewLine);
+                }
+                reader.Close();
+                //开始写入繁体文本
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(filePath, false, System.Text.Encoding.UTF8);
+                sw.Write(sb.ToString());
+                sw.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sb.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.textBox1.Text))
+            {
+                foreach (string itemPath in this.textBox1.Text.Split(','))
+                {
+                    ReadFile(itemPath, (line) =>
+                    {
+                        //进行简体转繁体
+                        string newValue = ChineseStringUtility.ToTraditional(line);
+                        return newValue;
+                    });
+                }
+                MessageBox.Show("转换成功");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.textBox1.Text))
+            {
+                foreach (string itemPath in this.textBox1.Text.Split(','))
+                {
+                    ReadFile(itemPath, (line) =>
+                    {
+                        string newValue = line.Replace("新宋体", "微软雅黑").Replace("宋体", "微软雅黑").Replace("Microsoft Sans Serif", "微软雅黑").Replace("Arial", "微软雅黑");
+                        return newValue;
+                    });
+                }
+                MessageBox.Show("转换成功");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.textBox1.Text))
+            {
+                foreach (string itemPath in this.textBox1.Text.Split(','))
+                {
+                    ReadFile(itemPath, (line) =>
+                    {
+                        if (line.Contains("<TextObject") && !line.Contains("Font="))
+                        {
+                            line = line.Replace("/>", " Font=\"微软雅黑, 9pt\"/>");
+                        }
+                        return line;
+                    });
+                }
+                MessageBox.Show("转换成功");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.textBox1.Text))
+            {
+                Regex regex = new Regex("Font=\"微软雅黑, ([1-9]\\d*\\.?\\d*)|(0\\.\\d*[1-9])\"");
+                foreach (string itemPath in this.textBox1.Text.Split(','))
+                {
+                    ReadFile(itemPath, (line) =>
+                    {
+                        if (line.Contains("Font"))
+                        {
+                            var groups = regex.Match(line).Groups;
+                            if (groups.Count != 0)
+                            {
+                                for (var i = 0; i < groups.Count; i++)
+                                {
+                                    if (!string.IsNullOrEmpty(groups[i].Value) && Regex.IsMatch(groups[i].Value, @"^[+-]?\d*[.]?\d*$"))
+                                    {
+                                        var num = Convert.ToDouble(groups[i].Value);
+                                        if (num >= 9)
+                                        {
+                                            line = line.Replace("微软雅黑, " + num, "微软雅黑, " + (num - 0.5));
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        return line;
+                    });
+                }
+                MessageBox.Show("转换成功");
+            }
+        }
+    }
+    /// <summary>
+    /// 中文字符工具类
+    /// </summary>
+    public static class ChineseStringUtility
+    {
+        private const int LOCALE_SYSTEM_DEFAULT = 0x0800;
+        private const int LCMAP_SIMPLIFIED_CHINESE = 0x02000000;
+        private const int LCMAP_TRADITIONAL_CHINESE = 0x04000000;
+
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int LCMapString(int Locale, int dwMapFlags, string lpSrcStr, int cchSrc, [Out] string lpDestStr, int cchDest);
+
+        /// <summary>
+        /// 将字符转换成简体中文
+        /// </summary>
+        /// <param name="source">输入要转换的字符串</param>
+        /// <returns>转换完成后的字符串</returns>
+        public static string ToSimplified(string source)
+        {
+            String target = new String(' ', source.Length);
+            int ret = LCMapString(LOCALE_SYSTEM_DEFAULT, LCMAP_SIMPLIFIED_CHINESE, source, source.Length, target, source.Length);
+            return target;
+        }
+
+        /// <summary>
+        /// 将字符转换为繁体中文
+        /// </summary>
+        /// <param name="source">输入要转换的字符串</param>
+        /// <returns>转换完成后的字符串</returns>
+        public static string ToTraditional(string source)
+        {
+            String target = new String(' ', source.Length);
+            int ret = LCMapString(LOCALE_SYSTEM_DEFAULT, LCMAP_TRADITIONAL_CHINESE, source, source.Length, target, source.Length);
+            return target;
+        }
+    }
+}
